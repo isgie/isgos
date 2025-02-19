6941281f6a816dfe322e22ac67c0ea2730b14576
Isgie <me@isgie.dev>
2025-02-19 02:06:09 -0500

    More refined packaging.

--- build.sh
+++ build.sh
@@ -10,8 +10,59 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y neovim
-dnf remove -y firefox firefox-langpacks
+
+remove_pkgs=(
+	alsa-firmware
+	firefox
+	firefox-langpacks
+	flatpak-spawn
+	google-noto-sans-balinese-fonts
+	google-noto-sans-cjk-fonts
+	google-noto-sans-javanese-fonts
+	google-noto-sans-sundanese-fonts
+	grub2-tools-extra
+	intel-vaapi-driver
+	just
+	libcamera
+	libcamera-gstreamer
+	libcamera-ipa
+	libcamera-tools
+	libratbag-ratbagd
+	libva-utils
+	lshw
+	net-tools
+	nvme-cli
+	nvtop
+	openrgb-udev-rules
+	oversteer-udev
+	pam-u2f
+	pam_yubico
+	pamu2fcfg
+	pipewire-libs-extra
+	pipewire-plugin-libcamera
+	powerstat
+	smartmontools
+	squashfs-tools
+	symlinks
+	tcpdump
+	tmux
+	traceroute
+	vim
+	wireguard-tools
+)
+dnf remove -y ${remove_pkgs[@]}
+
+install_pkgs=(
+	android-udev-rules
+	distrobox
+	duplicity
+	fzf
+	htop
+	neovim
+	solaar-udev
+	wl-clipboard
+)
+dnf install -y ${install_pkgs[@]}
 
 # Use a COPR Example:
 #
