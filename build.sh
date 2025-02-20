08c66e3b84cecfad0beb93b529f69fd2251a0e42
Isgie <me@isgie.dev>
2025-02-20 02:08:38 -0500

    Don't enable socket.

--- build.sh
+++ build.sh
@@ -35,7 +35,3 @@ dnf install -y ${install_pkgs[@]}
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
